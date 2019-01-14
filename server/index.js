0036f577f73f0c554aec7bfb344b9d8144f07748
cris93g <37388115+cris93g@users.noreply.github.com>
2019-01-13 21:01:49 -0600

    updated app to use auth security

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const app = express();
 const port = process.env.port || 3001;
 const cors = require("cors");
 const control = require("./controller/ProductsController");
-
+const passport = require("passport");
+const { getUser, strat, logout } = require(`${__dirname}/controller/authCtrl`);
 app.use(cors());
 app.use(json());
 
@@ -22,14 +23,46 @@ app.use(
 	})
 );
 
-massive(process.env.CONNECTION_STRING).then(dbinstance => {
-	app.set("db", dbinstance);
-});
 app.get("/api/test", (req, res, next) => {
 	res.sendStatus(200);
 });
+massive(process.env.CONNECTION_STRING).then(dbinstance => {
+	app.set("db", dbinstance);
+});
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strat);
+
+passport.serializeUser((user, done) => {
+	const db = app.get("db");
+	db.getUserByAuthid([user.id])
+		.then(response => {
+			if (!response[0]) {
+				db.addUserByAuthid([user.displayName, user.id, user.picture])
+					.then(res => done(null, res[0]))
+					.catch(console.log);
+			} else return done(null, response[0]);
+		})
+		.catch(console.log);
+});
+
+passport.deserializeUser((user, done) => done(null, user));
+
+app.get("/me", getUser);
+
+app.get(
+	"/login",
+	passport.authenticate("auth0", {
+		// successRedirect: "/",
+		successRedirect: "http://localhost:3000/#/",
+		// successRedirect: "/#/",
+		failureRedirect: "/login"
+	})
+);
+
 app.post("/api/addproducts", control.addToProducts);
 app.get("/api/products", control.getProducts);
+
 app.listen(port, () => {
 	console.log(`app is listening in port ${port}`);
 });
